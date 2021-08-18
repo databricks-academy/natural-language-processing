2a884d0429286953335aac3d55414b6cc5117c32
SireInsectus <SireInsectus>
2021-08-18 21:27:24 +0000

    Uploaded 1.6.0 from BDC

---
+++ Scalable-NLP-with-Spark-1.6.0/Version-1.6.0-IL.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md # Course: Scalable-NLP-with-Spark
+// MAGIC * Version 1.6.0-IL
+// MAGIC * Built 2021-08-18 21:21:11 UTC
+// MAGIC * Git revision: e67e4cb03001c3e95094d22d7abdbac9c122303c
+// MAGIC 
+// MAGIC Copyright © 2021 Databricks, Inc.
