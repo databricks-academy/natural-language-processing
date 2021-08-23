992340a5dea4253dd4d3c4250119d8495681cb82
SireInsectus <SireInsectus>
2021-08-23 18:02:41 +0000

    Checkpoint

--- Scalable-NLP-with-Apache-Spark-v1.6.1/Python/Version Info.py
+++ Scalable-NLP-with-Apache-Spark-v1.6.1/Python/Version Info.py
@@ -11,7 +11,7 @@
 # MAGIC 
 # MAGIC * Name: **Scalable NLP with Apache Spark**
 # MAGIC * Version:  **1.6.1**
-# MAGIC * Built On: **Aug 21, 2021 at 20:10:24 UTC**
+# MAGIC * Built On: **Aug 23, 2021 at 17:47:21 UTC**
 
 # COMMAND ----------
 
