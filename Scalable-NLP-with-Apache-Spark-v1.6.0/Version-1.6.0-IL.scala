be089a57a048919d91f8c3cb607824f8c28a9a2f
SireInsectus <SireInsectus>
2021-08-31 00:46:17 +0000

    Published 1.6.1

--- Scalable-NLP-with-Apache-Spark-v1.6.0/Version-1.6.0-IL.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: Scalable-NLP-with-Spark
-// MAGIC * Version 1.6.0-IL
-// MAGIC * Built 2021-08-18 21:31:15 UTC
-// MAGIC * Git revision: 9454398158cc415ad9a7766f8bbba09153f32887
-// MAGIC 
-// MAGIC Copyright © 2021 Databricks, Inc.
